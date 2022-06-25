41d32c1e9a543bf20c53b65a27089eef0f51d0f2
Apple <apple@Apples-MacBook-Pro.local>
2022-06-25 23:03:22 +0100

    screen 1

--- src/screens/AskADoctor/category/index.js
+++ src/screens/AskADoctor/category/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import colors from '../../../assets/theme/colors';
-import globalStyles from '../../../assets/global';
 
 const styles = StyleSheet.create({
   container: {
